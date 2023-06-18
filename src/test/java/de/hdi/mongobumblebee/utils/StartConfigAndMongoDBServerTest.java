1d0c24be8243f9f5bb975f615f5a01b63eaa3ba9
Mark Stehr <052265@hdi.de>
2023-06-18 11:44:06 +0200

    JUnit 5 tests enabled

--- src/test/java/de/hdi/mongobumblebee/utils/StartConfigAndMongoDBServerTest.java
+++ src/test/java/de/hdi/mongobumblebee/utils/StartConfigAndMongoDBServerTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 
 import com.mongodb.client.MongoClient;
 
-public class StartConfigAndMongoDBServerTest {
+class StartConfigAndMongoDBServerTest {
 	
 	@Test
 	void testMongoClient1() {
