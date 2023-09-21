f60265d48d08f70bdbefa8b31b43b7c9b87cc92b
Mark Stehr <052265@hdi.de>
2023-09-21 15:20:25 +0200

    Java module (issue #12)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,18 +6,14 @@ module de.hdi.mongobumblebee {
 	exports de.hdi.mongobumblebee;
 	
 	requires lombok;
-	requires java.compiler;
 	requires org.reflections;
 	requires transitive org.mongodb.driver.sync.client;
 	requires org.mongodb.driver.core;
 	requires org.slf4j;
 	requires transitive org.mongodb.bson;
-	requires spring.aop;
 	requires spring.beans;
 	requires spring.context;
 	requires transitive spring.core;
-	requires spring.data.commons;
 	requires spring.data.mongodb;
-	requires spring.tx;
 	
 }
